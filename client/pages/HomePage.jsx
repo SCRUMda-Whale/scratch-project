13b1ddfb35441f607372b67df8047cee1693c0a7
J Charles de Vera <jasoncdevera@icloud.com>
2021-09-13 14:09:16 -0700

    adding useHistory etc

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -1,67 +1,63 @@
 import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom';
-import BoardPage from './BoardPage';
+// import BoardPage from './BoardPage';
 import Board from '../components/Board';
 import '../scss/HomePage.scss';
 
-
 const HomePage = () => {
-
-  const [board, setBoard] = useState("");
+  const [board, setBoard] = useState('');
   const [boardList, setBoardList] = useState([]);
-  
-  const history = useHistory()
 
-  console.log("BOARD", boardList)
+  // const history = useHistory();
+
+  console.log('BOARD', boardList);
 
   const saveBoardHandler = () => {
     //make post request to backend
     //take the result and push it to boardlist
-    const newBoard = {title: board};
+    const newBoard = { title: board };
     fetch('/api', {
-    method: 'POST',
-    headers:  { 'Content-Type': 'application/json'},
-    body: JSON.stringify(newBoard),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newBoard),
     })
-      .then(data => data.json())
-      .then(res => setBoardList([...boardList, ...res]))
-    setBoard("");
-  }
+      .then((data) => data.json())
+      .then((res) => setBoardList([...boardList, ...res]));
+    setBoard('');
+  };
 
   useEffect(() => {
-    console.log("fetching boards")
+    console.log('fetching boards');
     fetch('/api')
-    .then(res => res.json())
-    .then(res => {
-      console.log("useeffect", res)
-      if(!res.isLoggedIn) {
-        console.log("isLoggedIn?", res.isLoggedIn)
-      
-      } else{
-      setBoardList([...boardList, ...res.boards]);
-      }
-    })
-    .catch(e => console.log(e));
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('useeffect', res);
+        if (!res.isLoggedIn) {
+          console.log('isLoggedIn?', res.isLoggedIn);
+        } else {
+          setBoardList([...boardList, ...res.boards]);
+        }
+      })
+      .catch((e) => console.log(e));
   }, []);
-  
-  
+
   // res = [{_id: "6098502bae510d80bb913ee4", title: "Scrum Board", __v: 0}, {_id: "6098502bae510d80bb913ee4", title: "Scrum Board", __v: 0}, {_id: "6098502bae510d80bb913ee4", title: "Scrum Board", __v: 0}]
   // cards for board components
   // input box and a button
 
-  return(
+  return (
     <div className="homePage">
       <h1>Project Boards</h1>
       <div className="board-container">
-      {boardList.map((board, i) => <Board key={`card${i}`} id={board._id} board={board.title} />)}
+        {boardList.map((board, i) => (
+          <Board key={`card${i}`} id={board._id} board={board.title} />
+        ))}
       </div>
 
       <div className="inputBox">
-        <input value={board} type='text' onChange={e => setBoard(e.target.value)}></input>
+        <input value={board} type="text" onChange={(e) => setBoard(e.target.value)}></input>
         {/* <button onClick={saveBoardHandler}>Create New Board</button> */}
-  
       </div>
-      
     </div>
   );
 };
